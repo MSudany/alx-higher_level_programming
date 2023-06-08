7ba440c16c80387361c757922f52ac3a7e0f4b21
msudany <mssodany2002@gmail.com>
2023-06-08 19:03:00 +0300

    Update 0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from add_0.py import add
+from add_0.py import add as add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
