c60558782270917f9657c8f68a71004d92049d6c
MSudany <mssodany2002@gmail.com>
2023-06-27 05:18:05 +0300

    Add 2.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    real = 0
+    try:
+        while count != x:
+            try:
+                print('{:d}'.format(my_list[n]))
+                count += 1
+                real += 1
+            except:
+                count += 1
+                continue
+    except:
+        None
+    print()
+    return real
