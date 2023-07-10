bd705432d433e0098b807f951776814f8190c0ab
msudany <mssodany2002@gmail.com>
2023-07-10 19:07:22 +0300

    Add 6.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''python oop'''
+
+
+class BaseGeometry:
+    '''class with 1 method'''
+    def area(self):
+    '''area method (not implemented yet)'''
+        raise Exception("area() is not implemented")
