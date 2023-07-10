d22d47b796226d411c59b017e701a0f834514f23
msudany <mssodany2002@gmail.com>
2023-07-10 21:42:31 +0300

    Indent 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,10 @@
 class MyList(list):
     '''class that inherits list'''
 
-        def __init__(self):
-            '''list initialization'''
-            super().__init__()
+    def __init__(self):
+        '''list initialization'''
+        super().__init__()
 
-        def print_sorted(self):
-            '''prints a list that is sorted ascendingly'''
-            print(sorted(self))
+    def print_sorted(self):
+        '''prints a list that is sorted ascendingly'''
+        print(sorted(self))
