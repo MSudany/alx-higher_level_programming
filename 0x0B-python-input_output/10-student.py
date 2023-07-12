5d83d2c77d43a0d44e69a2859fd2393d2828d727
msudany <mssodany2002@gmail.com>
2023-07-12 06:02:57 +0300

    Modify 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,11 +6,13 @@ class Student:
     '''defines a student'''
 
     def __init__(self, first_name, last_name, age):
+        '''intiation'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        '''to json'''
         if attrs is not None
             return self.vars(attrs)
         return self.__dict__
