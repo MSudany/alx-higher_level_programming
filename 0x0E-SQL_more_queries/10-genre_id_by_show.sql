70f25a76f58540c403be2b37966aa8c17a9464ce
msudany <mssodany2002@gmail.com>
2023-10-19 05:43:14 +0300

    add 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- ...
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+    INNER JOIN tv_shows_genres
+    ON tv_shows_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id
