f076ff54c93b4894c42e9ed123675fc715bc1332
msudany <mssodany2002@gmail.com>
2023-11-20 03:54:56 +0200

    add 6:9

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+...
+'''
+
+
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+
+    InstanceSession = sessionmaker(bind=engine)
+    session = InstanceSession()
+
+    for state in session.query(State).order_by(State.id):
+        print('{}: {}'.format(state.id, state.name))
+
+    session.close()
